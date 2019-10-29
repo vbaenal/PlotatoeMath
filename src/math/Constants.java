52d4fa57e5562d7565273fb31dc39af4cbf19fa3
Victor <victormage@hotmail.com>
2019-10-29 22:34:24 +0100

    Added some constants
    Added e, pi, tau and phi

---
+++ src/math/Constants.java
@@ -0,0 +1,8 @@
+package math;
+
+public class Constants {
+	public static final String E = "2.71828";
+	public static final String PI = "3.14159";
+	public static final String TAU = "6.28318";
+	public static final String PHI = "1.61803";
+}
