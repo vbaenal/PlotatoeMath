b0afa85924716b4296b59522b08adf76ce8c961a
Víctor Baena <victormage@hotmail.com>
2020-02-28 17:47:07 +0100

    Update Test.java

--- src/com/plotatoe/Test.java
+++ src/com/plotatoe/Test.java
@@ -11,7 +11,7 @@ import javax.imageio.ImageIO;
 public class Test {
 
 	public static void main(final String[] args) {
-    final String[] msg = { "(x-2)*(x+2)", "-2", "2", "0.3" }; 
+    		final String[] msg = { "(x-2)*(x+2)", "-2", "2", "0.3" }; 
 		try {
 			BufferedImage result = Plotatoe.getPlotAsBufferedImage(msg,1);
 			final File f = new File("test.png");
